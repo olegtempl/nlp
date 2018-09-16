0c5f029f84c7aacebbfedf53accc428ecca76dbd
olegtempl <linner_952@mail.ru>
2018-09-16 22:54:36 +0200

    ..

--- src/lib/help/firstParams.js
+++ src/lib/help/firstParams.js
@@ -2,11 +2,11 @@
  * @description Help for outputting the available keys to the console for transmission by the first argument
  */
 
-const firstParams = {
-  description: "",
-  example: "-d            text",
-  readme: "-r             text",
-  html: "-h               text"
-}
+let firstParams = {
+  description: '',
+  example: '-d            text',
+  readme: '-r             text',
+  html: '-h               text'
+};
 
-module.exports = firstParams
+module.exports = firstParams;
--- src/lib/help/secondParams.js
+++ src/lib/help/secondParams.js
@@ -2,11 +2,11 @@
  * @description Help for outputting the available keys to the console for transmission by the second argument
  */
 
-const secondParams = {
-  description: "",
-  example: "-d            text",
-  readme: "-r             text",
-  html: "-h               text"
-}
+const SECOND_PARAMS = {
+  description: '',
+  example: '-d            text',
+  readme: '-r             text',
+  html: '-h               text'
+};
 
-module.exports = secondParams
+module.exports = secondParams;
